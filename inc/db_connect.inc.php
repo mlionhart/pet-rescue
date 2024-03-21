df9f283aacc41e54422c54e2dc8db4b7b0214e26
mlionhart <lionrhart@gmail.com>
2024-03-21 07:11:17 -0600

    tested some things

--- inc/db_connect.inc.php
+++ inc/db_connect.inc.php
@@ -1,13 +1,11 @@
 <?php
 // db_connect.inc.php
-// Create a new connection to the database
-// (server name, username, password, database name)
+// Create a new connection to the MySQL server without selecting a database
 $db = new mysqli('localhost', 'root', '', 'petrescue');
 
-// If there was an error connecting to the database
+// If there was an error selecting the database
 if ($db->connect_error) {
-    $error = $db->connect_error;
-    echo $error;
+  die('Connect Error (' . $db->connect_errno . ') ' . $db->connect_error);
 }
 
 // Set the character encoding of the database connection to UTF-8
